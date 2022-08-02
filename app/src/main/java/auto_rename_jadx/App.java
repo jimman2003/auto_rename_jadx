35eb47271acdfe30309613b3b444147cc3ddd35a
jimman2003 <58356745+jimman2003@users.noreply.github.com>
2022-08-02 18:59:51 +0300

    Write to mappings

--- app/src/main/java/auto_rename_jadx/App.java
+++ app/src/main/java/auto_rename_jadx/App.java
@@ -6,31 +6,38 @@ import java.io.File;
 import jadx.api.JadxArgs;
 import jadx.api.JadxDecompiler;
 import jadx.api.JavaClass;
-import jadx.api.JavaMethod;
-import java.util.regex.Pattern;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.regex.Matcher;
-public class App {
-    public static void main(String[] args) {
-        JadxArgs jadxArgs = new JadxArgs();
-        String path = (args.length != 0) ? args[0] : "youtube.apk";
-        jadxArgs.setInputFile(new File(path));
-        Pattern className=Pattern.compile("return \"(\\w+)\\{");
-        //jadxArgs.setOutDir(new File("output"));
-        try (JadxDecompiler jadx = new JadxDecompiler(jadxArgs)) {
-            jadx.load();
-            for (JavaClass cls : jadx.getClasses()) {
-                    if (Character.isLowerCase(cls.getName().charAt(0))){
-                           if (cls.searchMethodByShortId("toString()Ljava/lang/String;") != null){
-                           Matcher matcher = className.matcher(cls.getCode());
-                           if (matcher.find()){System.out.println("CLASS " + cls.getName() + " " + matcher.group(1));}
-                    }        			    
-                }
-            }
-        }
-         catch (Exception e) {
-            e.printStackTrace();
-        }
-    
-    }
-}
+import java.util.regex.Pattern;
 
+public class App {
+  public static void main(String[] args) {
+    JadxArgs jadxArgs = new JadxArgs();
+    String path = (args.length != 0) ? args[0] : "youtube.apk";
+    jadxArgs.setInputFile(new File(path));
+    Pattern className = Pattern.compile("return \"(\\w+)\\{");
+    // jadxArgs.setOutDir(new File("output"));
+    try (JadxDecompiler jadx = new JadxDecompiler(jadxArgs)) {
+      jadx.load();
+      Path mappingsFolder=Files.createDirectories(Path.of("mappings/")); 
+      for (JavaClass cls : jadx.getClasses()) {
+        if (Character.isLowerCase(cls.getName().charAt(0))) {
+          if (cls.searchMethodByShortId("toString()Ljava/lang/String;") != null) {
+            Matcher classNameMatcher = className.matcher(cls.getCode());
+            if (classNameMatcher.find()) {
+              if (!cls.getName().equals("jgo")) {
+                String fileContent = "CLASS " + cls.getName() + " " + matcher.group(1);
+                System.out.println(fileContent);
+                Files.writeString(mappingsFolder.resolve(classNameMatcher.group(1)+".mapping"),fileContent,StandardOpenOption.CREATE);
+              }
+            }
+          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
